0c8b46b4a33a8ce9aa29c09bc3efacaf8cc400c2
Rangel Reale <rangelspam@gmail.com>
2023-10-10 18:53:43 -0300

    readme

--- sample/package/main.go
+++ sample/package/main.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"io"
+)
+
 // -type=Reader -package=io -overwrite=true -force-package=main
 
 func main() {
+	reader := NewDebugReader(
+		WithDebugReaderRead(func(debugCtx *DebugReaderContext, p []byte) (n int, err error) {
+			n = copy(p, []byte("test"))
+			return n, nil
+		}),
+	)
 
+	readInterface(reader)
+}
+
+func readInterface(r io.Reader) {
+	b := make([]byte, 10)
+
+	n, err := r.Read(b)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%d: %v", n, b)
 }
